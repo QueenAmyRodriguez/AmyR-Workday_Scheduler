23b16a7ae338095396b4e53bd24afa22adec53ee
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-01-22 20:52:15 -0700

    color code timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,53 @@
 // the current day is displayed at the top
-var currentDay = document.querySelector("#currentDay");
-var today = moment().format('MMMM Do YYYY');
-currentDay.innerHTML = today;
-console.log(today);
 
+//create a variable to store the element
+var currentDay = document.querySelector("#currentDay");
+//store class name results in variable
+var hoursList = document.querySelectorAll(".hour");
+
+console.log(hoursList);
+
+
+
+var textArea = document.querySelector("textarea");
+
+// creates a variable to store today's date
+var today = moment().format('dddd, MMMM Do, YYYY');
+
+// set the innerHTML of the element to reflect todays date on the page
+currentDay.innerHTML = today;
+
+var currentHour = moment();
 // there are time blocks from 9am - 5pm
 
+// create a function that will display business hours
+var businessHours = function () {
+    //store time in a variable
+    var bHours = moment().set('hour', 8);
+
+    // loop adds an hour to bhours and adds the time to the page
+    for (var i = 0; i < 8; i++) {
+        //add hours
+        hoursList[i].innerHTML = bHours.add(1, "hour").format("hA");
+    }
 // time blocks are color-coded to indicate past, present, and future
+    if (bHours.isBefore(currentHour)) {
+        $('textarea').addClass('past');
+    }
+    else if (bHours.isSame(currentHour)) {
+        $('textarea').addClass('present');
+    } else if (bHours.isAfter(currentHour)) {
+        $('textarea').addClass('future');
+    }
+}
+businessHours();
 
-// when i click on time block, I can enter event
+// when I click on the timeblock then i can enter an event 
 
-// when i click on the save button, the text is saved in local storage
 
-// when i refresh the page the saved events persist
+
+
+
+
+
+
